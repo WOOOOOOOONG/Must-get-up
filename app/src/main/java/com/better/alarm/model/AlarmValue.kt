bcb4bc385e9fc98dd93e87f3b82939d579cb0832
WOOOOOOOONG <sterme9@naver.com>
2023-01-01 17:49:34 +0900

    new alarm

---
+++ app/src/main/java/com/better/alarm/model/AlarmValue.kt
@@ -0,0 +1,76 @@
+package com.better.alarm.model
+
+import java.util.*
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.builtins.serializer
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+
+@Serializable
+data class AlarmValues(
+    val alarms: Map<Int, AlarmValue> = emptyMap(),
+)
+
+@Serializable
+data class AlarmValue(
+    @Serializable(with = CalendarSerializer::class)
+    val nextTime: Calendar = Calendar.getInstance().apply { timeInMillis = 0 },
+    val state: String = "DisabledState",
+    val id: Int = -1,
+    val isEnabled: Boolean = false,
+    val hour: Int = 0,
+    val minutes: Int = 0,
+    val penaltyTime: Int = -1,
+    val isPrealarm: Boolean = false,
+    val alarmtone: Alarmtone = Alarmtone.Default,
+    val isVibrate: Boolean = true,
+    val label: String = "",
+    val daysOfWeek: DaysOfWeek = DaysOfWeek(0),
+    val isDeleteAfterDismiss: Boolean = false,
+) {
+  val skipping
+    get() = state.contentEquals("SkippingSetState")
+
+  override fun toString(): String {
+    val box = if (isEnabled) "[x]" else "[ ]"
+    return "$id $box $hour:$minutes $daysOfWeek $label"
+  }
+
+  fun withState(name: String): AlarmValue = copy(state = name)
+  fun withIsEnabled(enabled: Boolean): AlarmValue = copy(isEnabled = enabled)
+  fun withNextTime(calendar: Calendar): AlarmValue = copy(nextTime = calendar)
+  fun withChangeData(data: AlarmValue) =
+      copy(
+          id = data.id,
+          isEnabled = data.isEnabled,
+          hour = data.hour,
+          minutes = data.minutes,
+          penaltyTime = data.penaltyTime,
+          isPrealarm = data.isPrealarm,
+          alarmtone = data.alarmtone,
+          isVibrate = data.isVibrate,
+          label = data.label,
+          daysOfWeek = data.daysOfWeek,
+          isDeleteAfterDismiss = data.isDeleteAfterDismiss,
+      )
+
+  fun withHour(hour: Int) = copy(hour = hour)
+  fun withDaysOfWeek(daysOfWeek: DaysOfWeek) = copy(daysOfWeek = daysOfWeek)
+  fun withPenalty(penaltyTime: Int) = copy(penaltyTime = penaltyTime)
+  fun withIsPrealarm(isPrealarm: Boolean) = copy(isPrealarm = isPrealarm)
+}
+
+object CalendarSerializer : KSerializer<Calendar> {
+  override val descriptor: SerialDescriptor
+    get() = Long.serializer().descriptor
+
+  override fun deserialize(decoder: Decoder): Calendar {
+    return Calendar.getInstance().apply { timeInMillis = decoder.decodeLong() }
+  }
+
+  override fun serialize(encoder: Encoder, value: Calendar) {
+    encoder.encodeLong(value.timeInMillis)
+  }
+}
